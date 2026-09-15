426b596d86efd202deecb58c66c6c60094a50e74
Andrew Levans <121060410+ALevansSamsung@users.noreply.github.com>
2026-09-15 16:57:04 -0500

    Add surface id on swapchain table (#34)

--- src/gfxr-db/internal/decode/vulkan_sqlite_prepared_statements.h
+++ src/gfxr-db/internal/decode/vulkan_sqlite_prepared_statements.h
@@ -1799,6 +1799,7 @@ struct VulkanSqlitePreparedStatements
     void InsertSwapchain(
         const format::HandleId swapchain,
         const format::HandleId device,
+        const std::optional<int64_t> surfaceId,
         const VkSwapchainCreateFlagsKHR flags,
         const uint32_t minImageCount,
         const VkFormat imageFormat,
